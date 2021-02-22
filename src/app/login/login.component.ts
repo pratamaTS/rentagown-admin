b66eb52dcc56a6da20730e9fbc8ac3e0b9de69b8
mukhlish kusuma <mukhlish1320@gmail.com>
2021-02-22 22:35:07 +0700

    - hanya admin yang bisa login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { ProfileService } from '../_services/profile.service';
+
 import { Router } from '@angular/router';
 
 @Component({
@@ -20,7 +22,8 @@ export class LoginComponent implements OnInit {
   errorMessage = '';
   roles: string = '';
 
-  constructor(private authService: Auth, private tokenStorage: TokenStorageService, public router: Router) { }
+  constructor(private authService: Auth, private profileService: ProfileService,
+    private tokenStorage: TokenStorageService, public router: Router) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -32,14 +35,25 @@ export class LoginComponent implements OnInit {
 
   doLogin(): void {
     const { email, password } = this.form;
-
     this.authService.login(email, password).subscribe(
       data => {
-        this.tokenStorage.saveToken(data.data.access_token);
-        this.tokenStorage.saveRefreshToken(data.data.refresh_token);
-        this.isLoginFailed = false;
-        this.isLoggedIn = true;
-        window.location.href = "dashboard"; //kadang error navigation gak bisa di klik dropdown
+        this.profileService.getProfile('Bearer', data.data.access_token).subscribe(
+          (d: any) => {
+            if (d.data.role == "Admin") {
+              this.tokenStorage.saveToken(data.data.access_token);
+              this.tokenStorage.saveRefreshToken(data.data.refresh_token);
+              this.isLoginFailed = false;
+              this.isLoggedIn = true;
+              window.location.href = "dashboard";
+            } else {
+              this.errorMessage = "Only Admin Allowed";
+            }
+          },
+          (profErr: any) => {
+            this.errorMessage = profErr.error.message;
+          }
+        )
+        //kadang error navigation gak bisa di klik dropdown
         // this.router.navigateByUrl('dashboard');
       },
       err => {
